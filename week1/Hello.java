6cd3ffdb8361c4505e9ebab8535c47f4164a4fa8
a894985555 <66674272+a894985555@users.noreply.github.com>
2020-09-22 23:57:11 +0800

    Update Hello.java

--- week1/Hello.java
+++ week1/Hello.java
@@ -1,13 +1,7 @@
 package week1;
 
-import java.util.Scanner;
-
 public class Hello {
 	public static void main(String[] args) {
-		Scanner cin = new Scanner(System.in);
-		String a = cin.nextLine();
-		int length = a.length();
-		System.out.println(length);
-		cin.close();
+		System.out.println("Hello World!");
 	}
 }
